6ba3be4d0f78a5691343b85f94915e17c7a640d3
abhinav khanna <kabhinav99@gmail.com>
2024-11-05 19:53:26 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NewPage from './NewPage'
+function App() {
+
+  return (
+    <>
+    <NewPage/>
+    </>
+  )
+}
+
+export default App
---
+++ src/NewPage.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+const NewPage = () => {
+    const [circlePosition, setCirclePosition] = useState(null);
+
+    const renderCircle = (x, y) => {
+        setCirclePosition({ x, y });
+    };
+
+    return (
+        <div 
+            className="min-h-[100vh] h-full w-full bg-gray-400 relative" 
+            onClick={(event) => renderCircle(event.clientX, event.clientY)}
+        >
+            hello 
+            {circlePosition && (
+                <div 
+                    className="bg-red-500 w-8 h-8 rounded-full absolute" 
+                    style={{ 
+                        left: circlePosition.x - 16, // Center the circle horizontally
+                        top: circlePosition.y - 16   // Center the circle vertically
+                    }}
+                ></div>
+            )}
+        </div>
+    );
+};
+
+export default NewPage;
